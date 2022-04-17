6ed9bfe7edfe1cff06bdc2a520097f9da6f59da6
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 18:35:45 +0600

    Update user controller to use jsonapi

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,27 +1,39 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    render json: User.all, code: 200, status: :ok
+    render jsonapi: User.all
   end
 
   def show
     @user = User.find(params[:id])
-    render json: UserSerializer.new(@user)
+    render jsonapi: @user
   end
 
   def create
     user = User.new user_params
 
     if user.save
-      render json: user, code: 201, status: :created
+      render jsonapi: user, code: '201', status: :created
     else
-      render json: user.errors, status: :unprocessable_entity, code: '422'
+      render jsonapi_errors: user.errors, status: :unprocessable_entity, code: '422'
     end
   end
 
   def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render jsonapi: user, code: '200', status: :ok
+    else
+      render jsonapi_errors: user.errors, status: :unprocessable_entity, code: '422'
+    end
   end
 
-  def delete
+  def destroy
+    user = User.find(params[:id])
+    if user.destroy
+      render jsonapi: user, code: '200', status: :ok
+    else
+      render jsonapi_errors: user.errors, status: :unprocessable_entity, code: '422'
+    end
   end
 
   private
