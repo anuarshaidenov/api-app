a5955cce8e5d86bec6cbfb1e304e4a3249039a4d
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 15:10:31 +0600

    Add shoulda-matchers

--- Gemfile
+++ Gemfile
@@ -49,6 +49,10 @@ group :development, :test do
   gem 'annotate'
 end
 
+group :test do
+  gem 'shoulda-matchers', '~> 5.0'
+end
+
 group :development do
   # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
   # gem "spring"
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'users/index'
-      get 'users/show'
-      get 'users/create'
-      get 'users/update'
-      get 'users/delete'
+      resources :users
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
