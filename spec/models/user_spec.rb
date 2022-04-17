785915a87eed30d4e8aff42bb55b60a2cff8e3c6
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 15:13:24 +0600

    Add validation test cases for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:email) }
+
+  it { should validate_presence_of(:fullname) }
+
+  it { should validate_uniqueness_of(:email) }
+
+  it { should validate_presence_of(:password) }
 end
