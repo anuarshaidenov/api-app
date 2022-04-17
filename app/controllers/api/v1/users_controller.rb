2d2250152957e247941a597dd81f50af8497427a
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 14:57:18 +0600

    Generate users controllet

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'users/index'
+      get 'users/show'
+      get 'users/create'
+      get 'users/update'
+      get 'users/delete'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
