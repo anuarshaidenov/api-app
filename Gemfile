da085a7b52634d24d6fb35e6d49ad294f8cf7f90
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 14:45:53 +0600

    Aded annotate and generated annotation

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'factory_bot_rails'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem 'debug', platforms: %i[mri mingw x64_mingw]
+  gem 'annotate'
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  fullname        :string
+#  email           :string
+#  password_digest :string
+#  gender          :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
 end
