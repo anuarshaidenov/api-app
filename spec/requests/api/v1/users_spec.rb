0fd893d313578b140b9509511bbe483a9a966e53
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-17 17:43:29 +0600

    Added test cases for users/show and post users/ paths

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -13,8 +13,27 @@ RSpec.describe "Api::V1::Users", type: :request do
 
   describe "GET /show" do
     it "returns http success" do
-      get "/api/v1/users/show"
-      expect(response).to have_http_status(:success)
+      user = create(:user)
+      get api_v1_user_path id: user.id
+      expect(response).to have_http_status(:ok)
+      expect(JSON.parse(response.body)).to include({ 'email' => user.email })
+    end
+  end
+
+  describe "POST /show" do
+    it "creates a new user" do
+      total_users = User.all.count
+      request_body = {
+        user: {
+          fullname: 'New User',
+          email: 'new@example.com',
+          password: 'djosdjodj'
+        }
+      }
+
+      post api_v1_users_path, params: request_body
+      expect(response).to have_http_status(201)
+      expect(User.all.count).to eq(total_users + 1)
     end
   end
 
