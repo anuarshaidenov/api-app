24e875ee333a64929dd08d3c12864c9bb58e4584
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 14:52:49 +0600

    Add a test case to expect auth token

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -10,5 +10,18 @@ RSpec.describe 'Api::V1::Sessions', type: :request do
       post api_v1_login_path, params: request_body
       expect(response).to have_http_status(200)
     end
+
+    it "expects auth token in response" do
+      user = create(:user)
+      request_body = {
+        user: { email: user.email, password: user.password}
+      }
+
+      post api_v1_login_path, params: request_body
+      result = JSON.parse(response.body)
+      token = JsonWebToken.encode(user_id: user.id)
+      expect(result["data"]["attributes"]["auth_token"]).to eq(token)
+      expect(response).to have_http_status(200)
+    end
   end
 end
