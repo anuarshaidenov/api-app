d4881d31c3d995c13123cba6ad6bad181b73d14d
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 14:42:04 +0600

    Add logic for login

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SessionsController < BaseController
+      # POST /api/v1/login
+      def login
+        user = User.find_by(email: login_params[:email])
+        if user.present? && user.authenticate(login_params[:password])
+          render jsonapi: user, status: :ok, code: '200'
+        else
+          render jsonapi_errors: [{ title: 'Invalid Email or Password' }],
+                 code: '401', status: :unauthorized
+        end
+      end
+
+      private
+        def login_params
+          params.require(:user).permit(:email, :password)
+        end
+    end
+  end
+end
