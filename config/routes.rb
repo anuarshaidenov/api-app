2babfc6c05fcb5f88c6dce60d12aa8bc4652fc28
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 15:39:00 +0600

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      patch 'change_password', to: 'users#change_password'
       post 'login', to: 'sessions#login'
     end
   end
