dfb450a4400913fdfd0726a7bad093c22dee80a4
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-04-18 14:41:25 +0600

    Add route for login

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      post 'login', to: 'sessions#login'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
